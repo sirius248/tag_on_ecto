1732cea670a70fa3603984b3ca4d28ea4b1f721b
Long Nguyen <long.polyglot@gmail.com>
2016-12-11 16:01:21 +0700

    Initial CI

---
+++ README.md
@@ -0,0 +1,24 @@
+# TagOnEcto
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `tag_on_ecto` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:tag_on_ecto, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `tag_on_ecto` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:tag_on_ecto]]
+    end
+    ```
+
---
+++ mix.exs
@@ -0,0 +1,37 @@
+defmodule TagOnEcto.Mixfile do
+  use Mix.Project
+
+  @version "0.0.1"
+
+  def project do
+    [app: :tag_on_ecto,
+     version: @version,
+     elixir: "~> 1.3",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :ecto, :timex_ecto]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [
+      {:ecto, "~> 2.0.6"},
+      {:timex_ecto, "~> 3.0.5"},
+    ]
+  end
+end
