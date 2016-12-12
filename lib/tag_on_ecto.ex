79fd6863d4105bed61de840805811a8c27d95b2d
Long Nguyen <long.polyglot@gmail.com>
2016-12-12 23:03:09 +0700

    Add template util

--- lib/tag_on_ecto.ex
+++ lib/tag_on_ecto.ex
@@ -1,2 +1,13 @@
 defmodule TagOnEcto do
+  defmacro __using__(_opts) do
+    quote do
+      import unquote(__MODULE__)
+    end
+  end
+
+  defmacro tag_on_ecto do
+    quote do
+      has_many :tags, TagOnEcto.Tag
+    end
+  end
 end
---
+++ lib/tag_on_ecto/util.ex
@@ -0,0 +1,12 @@
+defmodule TagOnEcto.Util do
+  alias TagOnEcto.{Tag, Tagging}
+
+  def create_tag do
+  end
+
+  def find_tag(tag_name) do
+  end
+
+  def tags_count do
+  end
+end
