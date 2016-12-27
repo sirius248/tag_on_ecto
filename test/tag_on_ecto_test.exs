37a699020d391264b4081c431a7ba9411fced30e
Long Nguyen <long.polyglot@gmail.com>
2016-12-27 11:59:21 +0700

    Add more test for tag

--- test/tag_on_ecto_test.exs
+++ test/tag_on_ecto_test.exs
@@ -7,7 +7,18 @@ defmodule TagOnEctoTest do
 
   test "create tag" do
     changeset = Tag.changeset(%Tag{}, %{name: "Elixir"})
-    user = TestRepo.insert! changeset
-    assert user.name == "Elixir"
+    tag = TestRepo.insert! changeset
+    assert tag.name == "Elixir"
+  end
+
+  test "cannot create tag with same name" do
+    changeset = Tag.changeset(%Tag{}, %{name: "functional"})
+    tag = TestRepo.insert! changeset
+    assert tag.name == "functional"
+
+    changeset = Tag.changeset(%Tag{}, %{name: "functional"})
+    {:error, changeset} = TestRepo.insert changeset
+    assert Keyword.has_key?(changeset.errors, :name) == true
+    assert Keyword.get(changeset.errors, :name) == {"has already been taken", []}
   end
 end
